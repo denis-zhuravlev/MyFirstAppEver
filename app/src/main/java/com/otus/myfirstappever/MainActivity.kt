297c2ec40ad17d13492b3726c2c6bf8e2f10131c
Denis <denis.zhuravlev.it@gmail.com>
2020-01-09 20:48:16 +0200

    Initial commit

---
+++ app/src/main/java/com/otus/myfirstappever/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.otus.myfirstappever
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        android.R.anim.slide_in_left
+        // todo: fdsfdsfsd
+    }
+}
